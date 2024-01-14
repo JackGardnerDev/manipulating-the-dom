e314ced2609cc2ad6a3a1fe11ea29b425dc28e7c
Jack Gardner <jackmcggardner@outlook.com>
2024-01-14 18:37:37 +1100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -84,6 +84,49 @@ function toggleSection(sectionId) {
     }
 }
 
+// Add class 'active' to navigation link when section is active
+function setActiveNavLink() {
+    const navLinks = document.querySelectorAll('.menu-link');
+    sections.forEach((section, index) => {
+        const box = section.getBoundingClientRect();
+        const navLink = navLinks[index];
+
+        if (box.top <= 200 && box.bottom > 0) {
+            navLink.classList.add('active');
+        } else {
+            navLink.classList.remove('active');
+        }
+    });
+}
+
+// Attach the setActiveNavLink function to the scroll event
+window.addEventListener('scroll', setActiveNavLink);
+
+// Scroll to a section when a link is clicked and update if state is active
+function scrollToSection() {
+    const navLinks = document.querySelectorAll('.menu-link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const sectionId = this.textContent.replace(/\s+/g, '').toLowerCase();
+            const targetSection = document.getElementById(sectionId);
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+
+            // Remove 'active' class from all links
+            navLinks.forEach(navLink => navLink.classList.remove('active'));
+
+            // Add 'active' class to the clicked link
+            this.classList.add('active');
+        });
+    });
+}
+
+// Set sections as active when seen and update if state is active
+document.addEventListener('scroll', () => {
+    setActiveSection();
+    setActiveNavLink();
+});
+
 // Build the navigation menu
 document.addEventListener('DOMContentLoaded', buildNav);
 
